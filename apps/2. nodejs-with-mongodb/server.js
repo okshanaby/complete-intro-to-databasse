3d77df830e726945eb79ce5d78d47e92082ac302
okshanaby <steamnature006@gmail.com>
2026-07-10 23:49:44 +0300

    Node.js with MongoDB — Simple Summary

---
+++ apps/2. nodejs-with-mongodb/server.js
@@ -0,0 +1,66 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+
+const connectionString =
+  process.env.MONGODB_CONNECTION_STRING ||
+  "mongodb://localhost:27017";
+
+async function init() {
+  const client =
+    new MongoClient(connectionString);
+
+  await client.connect();
+
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+
+    const db =
+      await client.db("adoption");
+
+    const collection =
+      db.collection("pets");
+
+    const pets =
+      await collection
+        .find(
+          {
+            $text: {
+              $search: req.query.search
+            }
+          },
+          {
+            projection: {
+              _id: 0
+            }
+          }
+        )
+        .sort({
+          score: {
+            $meta: "textScore"
+          }
+        })
+        .limit(10)
+        .toArray();
+
+    res.json({
+      status: "ok",
+      pets
+    }).end();
+  });
+
+  const PORT =
+    process.env.PORT || 3000;
+
+  app.use(express.static("./static"));
+
+  app.listen(PORT);
+
+  console.log(
+    `running on http://localhost:${PORT}`
+  );
+
+}
+
+init();
