678e59ad108c013741fdc1a588a3ea30c8825c91
okshanaby <steamnature006@gmail.com>
2026-07-14 12:38:12 +0300

    NodeJS with Neo4j — Simple Summary

---
+++ apps/3. nodejs-with-neo4j/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import neo4j from "neo4j-driver";
+
+const CONNECTION_STRING =
+  process.env.NEO4J_CONNECTION_STRING || "bolt://localhost:7687";
+
+const driver = neo4j.driver(CONNECTION_STRING);
+
+async function init() {
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+    const session = driver.session();
+    const result = await session.run(
+      `
+        MATCH path = shortestPath(
+            (First:Person {name: $person1 })-[*]-(Second:Person {name: $person2 })
+        )
+        UNWIND nodes(path) as node
+        RETURN coalesce(node.name, node.title) as text;
+    `,
+      {
+        person1: req.query.person1,
+        person2: req.query.person2,
+      },
+    );
+
+    res.json({
+      status: "ok",
+      path: result.records.map((record) => record.get("text")),
+    });
+
+    await session.close();
+  });
+
+  app.use(express.static("./static"));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT);
+  console.log(`running on http://localhost:${PORT}`);
+}
+init();
